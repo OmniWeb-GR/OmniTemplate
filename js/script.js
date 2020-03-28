624aa2c46b698de5f03eaf08d0dac78fb2969bb6
Andreas Kournoutas <andreaskournoutas@gmail.com>
2020-03-29 01:07:31 +0200

    Pagination component

--- js/script.js
+++ js/script.js
@@ -4,17 +4,15 @@ let deviceTier, deviceOrientation;
 
 jQuery(function() {
 	tierAndOrientation();
+	stylePagination();
 });
 
-jQuery(window).on("load", function (e) {
-	img4x3();
-	img16x9();
+jQuery(window).on('load', function (e) {
+	
 });
 
 jQuery(window).resize(function() {
 	tierAndOrientation();
-	img4x3();
-	img16x9();
 });
 
 jQuery(window).scroll(function() {
@@ -22,38 +20,44 @@ jQuery(window).scroll(function() {
 });
 
 function tierAndOrientation() {
-  	if (jQuery(".visible-xl").css('display') == 'block') {
-		deviceTier = "xl";
+  	if (jQuery('.visible-xl').css('display') == 'block') {
+		deviceTier = 'xl';
 	}
-	else if (jQuery(".visible-lg").css('display') == 'block') {
-		deviceTier = "lg";
+	else if (jQuery('.visible-lg').css('display') == 'block') {
+		deviceTier = 'lg';
 	}
-	else if (jQuery(".visible-md").css('display') == 'block') {
-		deviceTier = "md";
+	else if (jQuery('.visible-md').css('display') == 'block') {
+		deviceTier = 'md';
 	}
-	else if (jQuery(".visible-sm").css('display') == 'block') {
-		deviceTier = "sm";
+	else if (jQuery('.visible-sm').css('display') == 'block') {
+		deviceTier = 'sm';
 	}
-	else if (jQuery(".visible-xs").css('display') == 'block') {
-		deviceTier = "xs";
+	else if (jQuery('.visible-xs').css('display') == 'block') {
+		deviceTier = 'xs';
 	}
 	if (window.innerHeight >= window.innerWidth) {
-		deviceOrientation = "portrait";
+		deviceOrientation = 'portrait';
 	}
 	else {
-		deviceOrientation = "landscape";
+		deviceOrientation = 'landscape';
 	}
-	jQuery("body").removeClass('xl lg md sm xs portrait landscape').addClass(deviceTier).addClass(deviceOrientation);
+	jQuery('body').removeClass('xl lg md sm xs portrait landscape').addClass(deviceTier).addClass(deviceOrientation);
 }
 
 function img4x3() {
-	jQuery(".img-4x3").each(function() {
+	jQuery('.img-4x3').each(function() {
 		jQuery(this).height((jQuery(this).width() * 3) / 4);
 	});
 }
 
 function img16x9() {
-	jQuery(".img-16x9").each(function() {
+	jQuery('.img-16x9').each(function() {
 		jQuery(this).height((jQuery(this).width() * 9) / 16);
 	});
+}
+
+function stylePagination() {
+	jQuery('.pagination nav').unwrap();
+	jQuery('.pagenav').addClass('page-link');
+	jQuery('span.pagenav').parent().addClass('disabled');
 }
