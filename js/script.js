d7320d3c09aa006c15e5ceda97b0ef5844ec4b56
Andreas Kournoutas <andreaskournoutas@gmail.com>
2020-10-04 16:15:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ jQuery.noConflict();
 let deviceTier, deviceOrientation;
 
 jQuery(function() {
-	tierAndOrientation();
+	setTierAndOrientation();
 	stylePagination();
 });
 
@@ -12,41 +12,50 @@ jQuery(window).on('load', function (e) {
 });
 
 jQuery(window).resize(function() {
-	tierAndOrientation();
+	setTierAndOrientation();
 });
 
 jQuery(window).scroll(function() {
 
 });
 
-jQuery('#off-canvas__close-button').click(function() {
-	jQuery('#off-canvas').removeClass('off-canvas--open');
+jQuery('#off-canvas-open-button').click(function() {
+	openOffCanvas();
 });
 
-function tierAndOrientation() {
-  	if (jQuery('.visible-xl').css('display') == 'block') {
-		deviceTier = 'xl';
-	}
-	else if (jQuery('.visible-lg').css('display') == 'block') {
-		deviceTier = 'lg';
-	}
-	else if (jQuery('.visible-md').css('display') == 'block') {
-		deviceTier = 'md';
-	}
-	else if (jQuery('.visible-sm').css('display') == 'block') {
-		deviceTier = 'sm';
-	}
-	else if (jQuery('.visible-xs').css('display') == 'block') {
-		deviceTier = 'xs';
-	}
-	if (window.innerHeight >= window.innerWidth) {
-		deviceOrientation = 'portrait';
-	}
-	else {
-		deviceOrientation = 'landscape';
-	}
-	jQuery('body').removeClass('xl lg md sm xs portrait landscape').addClass(deviceTier).addClass(deviceOrientation);
+jQuery('#off-canvas-close-button').click(function() {
+	closeOffCanvas();
+});
+
+jQuery('#dark-layer').click(function() {
+	closeOffCanvas();
+});
+
+function setTierAndOrientation() {
+    if (jQuery('#visible-xs').css('display') == 'block') {
+        deviceTier = 'xs';
+    }
+    if (jQuery('#visible-sm').css('display') == 'block') {
+        deviceTier = 'sm';
+    }
+    if (jQuery('#visible-md').css('display') == 'block') {
+        deviceTier = 'md';
+    }
+    if (jQuery('#visible-lg').css('display') == 'block') {
+        deviceTier = 'lg';
+    }
+    if (jQuery('#visible-xl').css('display') == 'block') {
+        deviceTier = 'xl';
+    }
+    if (window.innerHeight >= window.innerWidth) {
+        deviceOrientation = 'portrait';
+    }
+    else {
+        deviceOrientation = 'landscape';
+    }
+    jQuery('body').removeClass('xs sm md lg xl portrait landscape').addClass(deviceTier).addClass(deviceOrientation);
 }
+
 
 function img4x3() {
 	jQuery('.img-4x3').each(function() {
@@ -64,4 +73,16 @@ function stylePagination() {
 	jQuery('.pagination aside').unwrap();
 	jQuery('.pagenav').addClass('page-link');
 	jQuery('span.pagenav').parent().addClass('disabled');
+}
+
+function openOffCanvas() {
+    jQuery('body').addClass('overflow-hidden');
+    jQuery("#dark-layer").removeClass('invisible');
+    jQuery('#off-canvas').addClass('off-canvas--open');
+}
+
+function closeOffCanvas() {
+    jQuery('#off-canvas').removeClass('off-canvas--open');
+    jQuery("#dark-layer").addClass('invisible');
+    jQuery('body').removeClass('overflow-hidden');
 }
